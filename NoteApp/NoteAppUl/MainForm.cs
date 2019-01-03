3b2ae17bb021ab9c5e158746ebe93369c525839f
Andrey Kalinin <andreykalinin.visual@yandex.ru>
2019-01-03 20:29:26 +0700

    Исправленны методы: Добавления, изменения, удаления.

--- NoteApp/NoteAppUl/MainForm.cs
+++ NoteApp/NoteAppUl/MainForm.cs
@@ -27,6 +27,7 @@ namespace NoteAppUl
             Project = ProjectManager.Load(Project,ProjectManager.FilePath);
             Project.NotesCollection = Project.SortedNotesCollection();
             ListViewNote = Project.NotesCollection;
+            ListCategoryNote = Project.NotesCollection;
             // Выгружаем все заметки из массива и добавляем в листбокс название.
             foreach (var note in ListViewNote)
             {               
@@ -37,20 +38,14 @@ namespace NoteAppUl
             {
                 CategoryComboBox.Items.Add(category);
             }
-            ProjectCurrentNote = ProjectManager.Load(ProjectCurrentNote, ProjectManager.FilePath);
-            NoteListBox.SelectedItem = ProjectCurrentNote.CurrentNote.Title;
-            textBox1.Text = ProjectCurrentNote.CurrentNote.Title;
-            CategoryTextBox.Text = ProjectCurrentNote.CurrentNote.CategoryNote.ToString();
-            CreatedDateTimePicker.Value = ProjectCurrentNote.CurrentNote.CreationTime;
-            ModifiedDateTimePicker.Value = ProjectCurrentNote.CurrentNote.LastChangeTime;
-            RichTextBox.Text = ProjectCurrentNote.CurrentNote.TextNote;
         }
 
         /// <summary>
         /// Поле для хранения отсортированного списка по категориям, внутри программы.
         /// </summary>
         private List<Note> ListViewNote;
-        
+        private List<Note> ListCategoryNote;
+
         // Кнопка элемента управления ListBox.
         private void NoteListBox_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -60,13 +55,6 @@ namespace NoteAppUl
             }
             else
             {
-                ProjectCurrentNote.CurrentNote.Title = Project.NotesCollection[NoteListBox.SelectedIndex].Title;
-                ProjectCurrentNote.CurrentNote.CategoryNote = Project.NotesCollection[NoteListBox.SelectedIndex].CategoryNote;
-                ProjectCurrentNote.CurrentNote.TextNote = Project.NotesCollection[NoteListBox.SelectedIndex].TextNote;
-                ProjectCurrentNote.CurrentNote.CreationTime = Project.NotesCollection[NoteListBox.SelectedIndex].CreationTime;
-                ProjectCurrentNote.CurrentNote.LastChangeTime = Project.NotesCollection[NoteListBox.SelectedIndex].LastChangeTime;
-                ProjectManager.Save(ProjectCurrentNote, ProjectManager.FilePath);
-                ProjectManager.Save(Project, ProjectManager.FilePath);
                 // При выборе заметки из листбокса всем полям правой панели присваиваются значения выбранной заметки.
                 textBox1.Text = ListViewNote[NoteListBox.SelectedIndex].Title;
                 CategoryTextBox.Text = ListViewNote[NoteListBox.SelectedIndex].CategoryNote.ToString();
@@ -90,9 +78,10 @@ namespace NoteAppUl
                 var upNote = addater.Note;
                 NoteListBox.Items.Clear();
                 Project.NotesCollection.Add(upNote);
+                ListViewNote = Project.NotesCollection;
                 ListViewNote = Project.SortedNotesCollection();
                 Project.NotesCollection = Project.SortedNotesCollection();
-                foreach (var note in Project.NotesCollection)
+                foreach (var note in ListViewNote)
                 {
                    NoteListBox.Items.Add(note.Title);
                 }
@@ -109,14 +98,22 @@ namespace NoteAppUl
             {
                 var sIndex = NoteListBox.SelectedIndex;
                 var inner = new NoteForm();
-                inner.Note = Project.NotesCollection[sIndex];
+                inner.Note = ListViewNote[sIndex];
                 var result = inner.ShowDialog(this);
                 if (result == DialogResult.OK)
                 {               
                     var upNote = inner.Note;
-                    NoteListBox.Items.Clear();
-                    Project.NotesCollection.RemoveAt(sIndex);
-                    Project.NotesCollection.Add(upNote);
+                    NoteListBox.Items.Clear(); 
+                    for(int i = 0; i < Project.NotesCollection.Count; i ++)
+                        if(ListViewNote[sIndex].Title == Project.NotesCollection[i].Title &&
+                            ListViewNote[sIndex].CategoryNote == Project.NotesCollection[i].CategoryNote &&
+                            ListViewNote[sIndex].TextNote == Project.NotesCollection[i].TextNote &&
+                            ListViewNote[sIndex].CreationTime == Project.NotesCollection[i].CreationTime &&
+                            ListViewNote[sIndex].LastChangeTime == Project.NotesCollection[i].LastChangeTime)
+                        {
+                            Project.NotesCollection.RemoveAt(i);
+                            Project.NotesCollection.Add(upNote);
+                        }
                     ListViewNote = Project.SortedNotesCollection();
                     Project.NotesCollection = Project.SortedNotesCollection();
                     foreach (var note in Project.NotesCollection)
@@ -143,28 +140,42 @@ namespace NoteAppUl
             {
 
                 // Условие в котором выскакивает MessegeBox, целью подтверждения удаления заметки.
-                if (MessageBox.Show("Do you really want to remove this note: " + Project.NotesCollection[NoteListBox.SelectedIndex].Title,
+                if (MessageBox.Show("Do you really want to remove this note: " + ListViewNote[NoteListBox.SelectedIndex].Title,
                   "DeleteNote", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
                 {
                     var selectedIndex = NoteListBox.SelectedIndex;
-                    Project.NotesCollection.RemoveAt(selectedIndex);
-                    // После удаления заметки, сразу производим сериализацию.
-                    ProjectManager.Save(Project, ProjectManager.FilePath);
-                    NoteListBox.Items.Clear();
-                    foreach (var note in Project.NotesCollection)
-                    {
-                        NoteListBox.Items.Add(note.Title);
-                    }
-                    // После удаления заметки, все поля правой части формы устанавливаем в начальное состояние.
-                    textBox1.Text = "";
-                    CategoryTextBox.Text = "";
-                    DateTime date1 = new DateTime(2018, 9, 1);
-                    DateTime date2 = new DateTime(2018, 9, 1);
-                    CreatedDateTimePicker.Value = date1;
-                    ModifiedDateTimePicker.Value = date2;
-                    RichTextBox.Text = "";
+                    for (int i = 0; i < Project.NotesCollection.Count; i++)
+                        if (ListViewNote[selectedIndex].Title == Project.NotesCollection[i].Title &&
+                            ListViewNote[selectedIndex].CategoryNote == Project.NotesCollection[i].CategoryNote &&
+                            ListViewNote[selectedIndex].TextNote == Project.NotesCollection[i].TextNote &&
+                            ListViewNote[selectedIndex].CreationTime == Project.NotesCollection[i].CreationTime &&
+                            ListViewNote[selectedIndex].LastChangeTime == Project.NotesCollection[i].LastChangeTime)
+                        {
+                            Project.NotesCollection.RemoveAt(i);
+                            ProjectManager.Save(Project, ProjectManager.FilePath);
+                            var Category = CategoryComboBox.SelectedItem;
+                            CategoryComboBox.SelectedIndex = 0;
+                            ListCategoryNote = Project.NotesCollection;
+                            NoteListBox.Items.Clear();
+                            foreach (var note in ListViewNote)
+                            {
+                                NoteListBox.Items.Add(note.Title);
+                            }
+                            // После удаления заметки, все поля правой части формы устанавливаем в начальное состояние.
+                            textBox1.Text = "";
+                            CategoryTextBox.Text = "";
+                            DateTime date1 = new DateTime(2018, 9, 1);
+                            DateTime date2 = new DateTime(2018, 9, 1);
+                            CreatedDateTimePicker.Value = date1;
+                            ModifiedDateTimePicker.Value = date2;
+                            RichTextBox.Text = "";
+                        }
                 }
-            }         
+            }  
+            else
+            {
+                return;
+            }
         }
 
         /// <summary>
